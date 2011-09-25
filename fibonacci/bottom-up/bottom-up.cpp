2f9204fc3d3eece0de72ba7037f0a78f295a0688
Michael <me@michael-ling.com>
2011-09-24 22:00:58 -0400

    added additional fibonacci programs

---
+++ fibonacci/bottom-up/bottom-up.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bottom-up.cpp
+ *
+ *    Description:  The bottom-up implementation for finding an nth fibonacci number
+ *
+ *        Version:  1.0
+ *        Created:  11-09-24 08:50:14 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  michael ling (), me@michael-ling.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+
+int main ( int argc, char *argv[] )
+{
+		
+	return 0;
+}	
+
+		
---
+++ fibonacci/recursive-squaring/recursive-squaring.cpp
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  	recursive-squaring.cpp
+ *
+ *    Description:  	Implementatino of the algorithm to find the nth
+ *    			fibonacci number using matrix multiplication
+ *
+ *        Version:  	1.0
+ *        Created:  	11-09-24 09:29:09 PM
+ *       Revision:  	none
+ *       Compiler:  	gcc
+ *
+ *         Author:  	michael ling (), me@michael-ling.com
+ *        Company:  	
+ *
+ * =====================================================================================
+ */
+
+#include	<stdlib.h>
+
+int main ( int argc, char *argv[] )
+{
+	int base[2][2] = { {1, 1}, {1, 0} } ;   /* essential matrix  */
+
+	
+	
+	return 0;
+}			
